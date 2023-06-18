48ece2089858a0dd32aefb55aaea1c8acc4bae72
ocztoa <mins04313@gmail.com>
2023-06-18 12:15:48 +0900

    :memo:update

--- io/a.py
+++ io/a.py
@@ -7,6 +7,11 @@
 # 입출력
 # 변수 상수
 
+# 변수의 type
+# int : 10, 20, -20, 0
+# float : 1.4, 3.14
+# str : "안녕하세요", 'asdf'
+
 # 수학 : '=' << 비교연산자
 # x = 20
 
@@ -14,5 +19,29 @@
 # x = 20 :: x에 20을 대입한다
 # <----
 
+# 표준입출력
+
 # 입력
-a = input()
+# a = input()
+# a = int(input()) : 숫자를 입력
+# 여러 줄 입력
+# a = int(input())
+# b = int(input())
+
+# 한줄에 여러개가 들어올때 : 10_20
+# a, b = map(int, input().split())
+# 10_30_50_69
+# a, b, c, d = map(int, input().split())
+# hello_wolrd
+# a, b = map(str, input().split())
+
+# 출력
+# print("asdf")
+# # print(10 + 13)
+# print(13, "hello", sep='-')
+# print("1515", end='\n')
+# print("1515", end=' ')
+# print("15")
+# print("13" + "5")
+
+print("Hello World!")
